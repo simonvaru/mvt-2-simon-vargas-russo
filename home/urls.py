5289a1bccbe3a9e575e1f5a3be147abb459a2e1e
simonvaru <pennywise1540b@hotmail.com>
2022-10-16 13:03:05 -0300

    commit 23, modificado urls.py(comentado)

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,12 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hola/', views.hola, name='hola'),
-    path('fecha/', views.fecha, name='fecha'),
-    path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
+    # path('hola/', views.hola, name='hola'),
+    # path('fecha/', views.fecha, name='fecha'),
+    # path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
     # path('mi-template/', views.mi_template, name='mi_template'),
-    path('tu-template/<str:nombre>', views.tu_template),
-    path('prueba-template/', views.prueba_template),
+    # path('tu-template/<str:nombre>', views.tu_template),
+    # path('prueba-template/', views.prueba_template),
     path('ver-personas/', views.ver_personas, name='ver_personas'),
     path('crear-persona/', views.crear_persona, name='crear_persona'),
 ]
