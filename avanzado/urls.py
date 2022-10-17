58da2508fec93631f0c4344929883df17bdcc7a9
simonvaru <pennywise1540b@hotmail.com>
2022-10-17 18:29:20 -0300

    commit 26, 1/2 clase 22

--- avanzado/urls.py
+++ avanzado/urls.py
@@ -3,5 +3,5 @@ from avanzado import views
 
 urlpatterns = [
     path('mascotas/', views.ver_mascotas, name='ver_mascotas'),
-    path('mascotas/crear/', views.crear_mascotas, name='crear_mascotas')
+    path('mascotas/crear/', views.crear_mascotas, name='crear_mascota')
 ]
--- avanzado/views.py
+++ avanzado/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from avanzado.models import Mascota
+from avanzado.forms import MascotaFormulario
 
-
-# Create your views here.
 def ver_mascotas(request):
     
     mascotas = Mascota.objects.all()
     
-    return render(request, 'ver_mascotas.html', {'mascotas': mascotas})
+    return render(request, 'avanzado/ver_mascotas.html', {'mascotas': mascotas})
+
+def crear_mascotas(request):
+    
+    if request.method == 'POST':
+        formulario = MascotaFormulario(request.POST) 
+     
+
+        if formulario.is_valid():
+            datos=formulario.cleaned_data
+           
+            mascota = Mascota(
+               nombre=datos['nombre'], 
+               tipo=datos['tipo'], 
+               edad=datos['edad'], 
+               fecha_nacimiento=datos['fecha_nacimiento']
+            )
+            mascota.save()
+            return redirect('ver_mascotas')
+    
+    formulario = MascotaFormulario()
+    
+    return render(request, 'avanzado/crear_mascota.html', {'formulario': formulario})
