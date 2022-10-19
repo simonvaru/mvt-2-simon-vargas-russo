05036267fcb01ffdb8e24979366fae6974d513fc
simonvaru <pennywise1540b@hotmail.com>
2022-10-19 20:22:35 -0300

    commit 32, 1/2 clase 23, login account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
+from re import template
 from django.urls import path
 from accounts import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', views.mi_login, name='login') 
+    path('login/', views.mi_login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'), 
 ]
