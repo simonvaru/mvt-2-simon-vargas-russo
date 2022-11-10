b9db4d4f615ce03dde9c371e58e19e6eb79fedef
simonvaru <pennywise1540b@hotmail.com>
2022-11-10 13:23:05 -0300

    commit 41, cambios minimos

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class ExtensionUsuario(models.Model):
     # descripcion
     # email
+    # avatar = models.ImageField(upload_to='avatares', height_field=50, width_field=50, null=True, blank=True)
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
     # user = models.ForeignKey(User, on_delete=models.CASCADE) 
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
