d747a121ddd87b71c0c2c152ae799534032a997e
simonvaru <pennywise1540b@hotmail.com>
2022-11-10 12:24:52 -0300

    commit 40, ver lista de autos sin problemas, crear con falla, DDBB con error (en vez de avanzado_vehiculo, esta avanzado_auto)

--- avanzado/forms.py
+++ avanzado/forms.py
@@ -7,6 +7,12 @@ class MascotaFormulario(forms.Form):
     edad = forms.IntegerField()
     fecha_nacimiento = forms.DateField()
     
-    
+class Vehiculo(forms.Form):
+    modelo = forms.CharField(max_length=20)
+    marca  = forms.CharField(max_length=20)
+    cant_puertas = forms.IntegerField()
+    color = forms.CharField(max_length=20)
+    chasis = forms.CharField(max_length=20)
+        
     
     
